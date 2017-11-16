3c24aed202b35cae07709fe9e42e9866a08e1d31
yxnyxnyxnyxnyxnyxn <yxnyxnyxnyxnyxnyxn@gmail.com>
2017-11-15 21:11:01 -0500

    add addEdge function and connect to neo4j server

--- network.py
+++ network.py
@@ -1,18 +1,28 @@
 # NOTES: calculatedFrequency is going to be in terms of hours or days? currently in hours
 
 from py2neo import Graph, Node, Relationship
-
+from py2neo.server import GraphSever
 class Network():
 	def __init__(self):
+                self.server = GraphSever()
+                self.server.start()
 		self.graphInstance = Graph()
 		self.tx = self.graphInstance.begin()
 
 	def addNode(self, link, dateLastUpdated, frequency):
 		calculatedFrequency = convertFrequencyToHours(frequency)
 		n = Node(link, dateLastUpdated = dateLastUpdated, frequency = frequency, calculatedFrequency = calculatedFrequency)
-		tx.create(n)
+		self.tx.create(n)
+        
+        def addEdge(self, nodeu, nodev, relation):
+                self.tx.create(Relationship(nodeu,"links to",nodev,tag = relation))
+
+
+            
+
 
 	# def addOutlink()
+        # We can use 0 to indicate always and -1 to indicate never
 	def convertFrequencyToHours(frequency):
 		if (frequency == "always" or frequency == ""):		# Unsure what to do for this?
 			return 0
