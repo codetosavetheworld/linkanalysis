cc885a8b95d2a59558efc8985aa36909e24943b9
yxnyxnyxnyxnyxnyxn <yxnyxnyxnyxnyxnyxn@gmail.com>
2017-11-27 15:44:06 -0500

    updated network

--- link_analysis/network.py
+++ link_analysis/network.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream:link_analysis/network.py
 
 from py2neo import Graph, Node, Relationship
 # from py2neo.server import GraphSever
@@ -26,9 +27,11 @@ class Network():
             n.push()
 
     
-    def add_edge(self, node_u, node_v, relationship):
+    def add_edge(self, node_u, node_v_link, relationship):
         #if the relationship not exist create new edge
         #else update the tag
+        self.add_node(node_v_link,"","","")
+        node_v = self.get_node(node_v_link)
         if not self.check_relationship_exist(node_u,node_v):
             self.graph_instance.create(Relationship(node_u, "links to", node_v, tag = relationship))
         else:
@@ -82,3 +85,83 @@ n.add_edge(n1,n2,"<h3>")
 
 
 
+=======
+
+from py2neo import Graph, Node, Relationship
+# from py2neo.server import GraphSever
+
+class Network():
+    def __init__(self):
+        # self.server = GraphSever()
+        # self.server.start()
+        self.graph_instance = Graph()
+
+
+    def add_node(self, link, date_last_updated, frequency):
+        # check if the node exist
+        # if node not exist create a new node
+        # if node exist update the node
+        calculated_frequency = convert_frequency_to_hours(frequency)
+      
+        if (not self.check_node_exist(link)):
+            n = Node(link, date_last_updated = date_last_updated, frequency = frequency, calculated_frequency = calculated_frequency)
+            self.graph_instance.create(n)
+        else:
+            n = self.graph_instance.find_one(link)
+            n["date_last_updated"] = date_last_updated
+            n["calculated_frequency"] = calculated_frequency
+            n["frequency"] = frequency
+            n.push()
+
+    
+    def add_edge(self, node_u, node_v_link, relationship):
+        #if the relationship not exist create new edge
+        #else update the tag
+        self.add_node(node_v_link, "","","")
+        node_v = self.get_node(node_v_link)
+        if not self.check_relationship_exist(node_u,node_v):
+            self.graph_instance.create(Relationship(node_u, "links to", node_v, tag = relationship))
+        else:
+            for l in list(self.graph_instance.match(start_node = node_u, end_node = node_v, rel_type = "links to")):
+                l["tag"] = relationship
+                l.push()
+
+
+    
+    #def delete_edge(self,node_u,node_v,relationship):
+    
+    
+
+    def check_node_exist(self, link):
+        return len(list(self.graph_instance.find(link))) != 0
+
+    def check_relationship_exist(self,node_u, node_v):
+        return len(list(self.graph_instance.match(start_node = node_u, end_node = node_v, rel_type = "links to"))) > 0
+    
+    def get_node(self,link):
+        return self.graph_instance.find_one(link)
+
+
+
+def convert_frequency_to_hours(frequency):
+    if (frequency == "always" or frequency == ""):
+        return 0
+    if (frequency == "hourly"):
+        return 1
+    if (frequency == "daily"):
+        return 24
+    if (frequency == "weekly"):
+        return 7*24
+    if (frequency == "monthly"):
+        return 30*24
+    if (frequency == "yearly"):
+        return 365*24
+    if (frequency == "never"):
+        return -1
+
+
+
+
+
+
+>>>>>>> Stashed changes:network.py
---
+++ tests/test_addnodes_addedges.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template, request
+import sys
+import json
+import requests
+import network
+
+
+
+
+
+
+def parse_session_information(session_info):
+    sessionID = session_info["sessionID"]
+    webpage = session_info["webpages"][0]["link"]
+    date_last_updated = session_info["webpages"][0]["date_last_updated"]
+    frequency = session_info["webpages"][0]["frequency"]
+    outlinks = session_info["webpages"][0]["outlinks"]
+    outlinks_webpage = outlinks[0]["link"]
+    #failed_webpages = session_info["failed_webpages"][0]
+
+
+    #add nodes
+    test_network = network.Network()
+    test_network.add_node(webpage,date_last_updated,frequency)
+    test_network.add_edge(test_network.get_node(webpage),outlinks_webpage,"tag1")
+
+
+
+
+
+
+
+
+# --- Functions for premilimary testing (will be removed) ---
+def make_sample_session_info():
+    sample_session_info = {}
+    sample_session_info["sessionID"] = 1
+
+    sample_link = {}
+    sample_link["link"] = "www.example.com"
+    sample_link["date_last_updated"] = "2017-05-20"
+    sample_link["frequency"] = "daily"
+
+    sample_outlink = {}
+    sample_outlink["link"] = "www.example2.com"
+    sample_outlink["tags"] = ["h1"]
+    sample_link["outlinks"] = [sample_outlink]
+
+    sample_session_info["webpages"] = [sample_link]
+    sample_session_info["failed_webpages"] = ["www.example3.com"]
+
+    json_data = json.dumps(sample_session_info)
+    return json_data
+
+
+
+
+def main():
+    session_info = make_sample_session_info()
+    session_info = json.loads(session_info)
+    parse_session_information(session_info)
+
+
+
+main()
